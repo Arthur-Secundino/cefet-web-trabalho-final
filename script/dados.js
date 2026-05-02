e007fba978dd22e3e7b4d476446280317e600433
Arthur-Secundino <artsecundino@gmail.com>
2026-05-02 20:14:47 -0300

    Adição de imagens e ínicio do js

---
+++ script/dados.js
@@ -0,0 +1,75 @@
+const livrosEmAltaNaComunidade = [
+    {
+        capa: "../imgs/crime_castigo.png",
+        titulo: "Crime e Castigo",
+        descricao: "Crime e Castigo é um romance do escritor Fiódor Dostoiévski..."
+    },
+    {
+        capa: "../imgs/crime_castigo.png",
+        titulo: "Crime e Castigo",
+        descricao: "Crime e Castigo é um romance do escritor Fiódor Dostoiévski..."
+    },
+    {
+        capa: "../imgs/crime_castigo.png",
+        titulo: "Crime e Castigo",
+        descricao: "Crime e Castigo é um romance do escritor Fiódor Dostoiévski..."
+    }
+];
+
+const prateleirasPraSeInsipirar = [
+    {
+        imagem: "../imgs/terror_classico.png",
+        nome: "Terror Clássico",
+        descricao: "Os maiores títulos do terror de todos os tempos, histórias que traumatizaram gerações."
+    },
+    {
+        imagem: "../imgs/terror_classico.png",
+        nome: "Terror Clássico",
+        descricao: "Os maiores títulos do terror de todos os tempos, histórias que traumatizaram gerações."
+    }
+];
+
+const reviewsRecentes = [
+    {
+        titulo: "Definitivamente um livro",
+        livro: {
+            capa: "../imgs/crime_castigo.png",
+            titulo: "Crime e Castigo",
+            nota: "4.9"
+        },
+        usuario: {
+            avatar: "../imgs/avatar.png",
+            nome: "Arthur",
+            arroba: "@Tuzinho"
+        },
+        review: "Clássico da Literatura Russa, simplesmente fantástico."
+    },
+    {
+        titulo: "Definitivamente um livro",
+        livro: {
+            capa: "../imgs/crime_castigo.png",
+            titulo: "Crime e Castigo",
+            nota: "4.9"
+        },
+        usuario: {
+            avatar: "../imgs/avatar.png",
+            nome: "Arthur",
+            arroba: "@Tuzinho"
+        },
+        review: "Clássico da Literatura Russa, simplesmente fantástico."
+    },
+    {
+        titulo: "Definitivamente um livro",
+        livro: {
+            capa: "../imgs/crime_castigo.png",
+            titulo: "Crime e Castigo",
+            nota: "4.9"
+        },
+        usuario: {
+            avatar: "../imgs/avatar.png",
+            nome: "Arthur",
+            arroba: "@Tuzinho"
+        },
+        review: "Clássico da Literatura Russa, simplesmente fantástico."
+    }
+];
