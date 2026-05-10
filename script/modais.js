b5ab18fdf77cac5038a10052033d900ee8d87319
Arthur-Secundino <artsecundino@gmail.com>
2026-05-10 18:01:15 -0300

    Continuação dos modais da landing page

--- script/modais.js
+++ script/modais.js
@@ -4,8 +4,18 @@ const botaoLoginEl = document.querySelector("#botao_login");
 const modalCriarContaEl = document.querySelector("#criar_conta");
 const modalLogarEl = document.querySelector("#logar");
 
+const botaoEnviarDadosCadastroEl = document.querySelector("#enviar_dados_cadastro");
+const botaoEnviarDadosLoginEl = document.querySelector("#enviar_dados_login");
+
 const botaoCancelarCadastroEl = document.querySelector("#cancelar_cadastro");
 const botaoCancelarLoginEl = document.querySelector("#cancelar_login");
+
+const inputNomeUsuarioCadastroEl = document.querySelector("#nome_usuario_cadastro");
+const inputEmailCadastroEl = document.querySelector("#email_cadastro");
+const inputSenhaCadastroEl = document.querySelector("#senha_cadastro");
+
+const inputEmailLoginEl = document.querySelector("#email_login");
+const inputSenhaLoginEl = document.querySelector("#senha_login");
 
 botaoCriarContaEl.addEventListener("click", () => {
     modalCriarContaEl.showModal();
@@ -13,6 +23,18 @@ botaoCriarContaEl.addEventListener("click", () => {
 
 botaoLoginEl.addEventListener("click", () => {
     modalLogarEl.showModal();
+});
+
+botaoEnviarDadosCadastroEl.addEventListener("click", () => {
+    if(inputNomeUsuarioCadastroEl.value !== "" && inputEmailCadastroEl.value !== "" && inputSenhaCadastroEl.value !== ""){
+        window.location.href = "./pages/home.html";
+    }
+});
+
+botaoEnviarDadosLoginEl.addEventListener("click", () => {
+    if(inputEmailLoginEl.value !== "" && inputSenhaLoginEl.value !== ""){
+        window.location.href = "./pages/home.html";
+    }
 });
 
 botaoCancelarCadastroEl.addEventListener("click", () => {
